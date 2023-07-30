c5daec8a9d085a0da5334059bdf06f5736efbdfe
NxtNinja <136341993+NxtNinja@users.noreply.github.com>
2023-07-30 10:23:52 +0530

    updated code

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,14 @@
 import "regenerator-runtime/runtime";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Vocalization.Pro</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
